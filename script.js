986c05b50e2e83fd198e9139cfea86eb21a18245
apolloplease <delgado_theon@yahoo.com>
2022-03-16 00:06:43 -0400

    Added new functionality

--- script.js
+++ script.js
@@ -9,16 +9,63 @@ function computerPlay()
 
     if(choice == 1)
     {
-        return "Rock";
+        return "Rock".toLowerCase();
     }
     if(choice == 2)
     {
-        return "Paper";
+        return "Paper".toLowerCase();
     }
     if(choice == 3)
     {
-        return "Scissors";
+        return "Scissors".toLowerCase();
     }
 }
+
+function Round(playerSelection, computerSelection)
+{
+    calculateWinner(playerSelection, computerSelection);   
+}
+
+function calculateWinner(a, b)
+{
+    if(a == b)
+    {
+        window.alert("Tie!");
+    }
+
+    if(a == "rock" && b == "paper")
+    {
+        window.alert("Computer Wins!");
+    }
+    if(a == "rock" && b == "scissors")
+    {
+        window.alert("Player wins!");
+    }
+
+    if(a == "paper" && b == "rock")
+    {
+        window.alert("Player wins!");
+    }
+    if(a == "paper" && b == "scissors")
+    {
+        window.alert("Computer wins!");
+    }
+
+    if(a == "scissors" && b == "rock")
+    {
+        window.alert("Computer wins!");
+    }
+    if(a == "scissors" && b == "paper")
+    {
+        window.alert("Player wins!");
+    }
+}
+
+function getPlayerInput()
+{
+    window.prompt("Enter input: ");
+}
+
+Round(getPlayerInput(), computerPlay());
 
 
